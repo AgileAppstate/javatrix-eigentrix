05c918cb97e403cd89e694c0593e4845d938d7dd
Jay Fenwick <fenwickjb@appstate.edu>
2021-09-27 16:42:07 -0400

    empty Matrix so things compile

---
+++ Matrix.java
@@ -0,0 +1,16 @@
+
+/*
+public Matrix(int m,
+              int n,
+              double s)
+Construct an m-by-n constant matrix.
+Parameters:
+m - Number of rows.
+n - Number of colums.
+s - Fill the matrix with this scalar value.
+ */
+
+public class Matrix {
+    public Matrix(int m, int n, double s) {
+    }
+}
--- TestConstructors.java
+++ TestConstructors.java
@@ -3,6 +3,7 @@
  */
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class TestConstructors {
