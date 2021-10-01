40b3d6d5855dbb0ee48c70cf640509f10ea3491b
Jay Fenwick <fenwickjb@appstate.edu>
2021-10-01 13:42:28 -0400

    done

--- src/TestPrinters.java
+++ src/TestPrinters.java
@@ -53,6 +53,40 @@ public class TestPrinters {
 	  restoreOut();
       }
 
+      @Test
+      public void testPrint2_positive() {
+	  // Tests print(int w, int d)
+	  // Tests "normal" usages...
+
+	  redirectOut();
+
+	  // Test #1: print(5,2)
+	  Matrix m1 = new Matrix(3, 2, 2.12345);
+	  m1.print(5,2);
+	  String actualOutput = getOutput();
+	  String expectedOutput = "  2.12  2.12\n  2.12  2.12\n  2.12  2.12\n";
+	  assertEquals(expectedOutput, actualOutput);
+
+	  restoreOut();
+      }
+
+      @Test
+      public void testPrint3_positive() {
+	  // Tests print(int w, int d)
+	  // Tests "normal" usages...
+
+	  redirectOut();
+
+	  // Test #1: print(5,2)
+	  Matrix m1 = new Matrix(2, 3, 2.155);
+	  m1.print(5,2);
+	  String actualOutput = getOutput();
+	  String expectedOutput = "  2.16  2.16  2.16\n  2.16  2.16  2.16\n";
+	  assertEquals(expectedOutput, actualOutput);
+
+	  restoreOut();
+      }
+
 
 
 }
