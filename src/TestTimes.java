9ea5fb24669e4b1e6cac74989c8377101a87810c
Abdel Issa <issaa@appstate.edu>
2021-10-02 23:24:02 -0400

    Fix Checkstyle

---
+++ src/TestTimes.java
@@ -0,0 +1,52 @@
+/* 
+ * JUnit5 test class
+ */
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import javatrix.*;
+
+public class TestTimes {
+	Matrix baseMatrix; 
+
+	@BeforeEach
+	public void init(){
+		baseMatrix = new Matrix(5,5,2);
+	}
+
+	@Test
+	public void testTimeCorrect(){
+		Matrix multiply = new Matrix(5,1,5);
+		multiply = baseMatrix.times(multiply);
+		Matrix answer = new Matrix(5,1,50);
+
+		multiply.print(2,1);
+		answer.print(2,1);
+
+		
+		assertTrue(multiply.equals(answer));
+	}
+
+	@Test
+	public void testTimeIllegalInput(){
+		boolean result1 = true;
+
+		Matrix badMatrix = new Matrix(3, 3, 2);
+		try {
+	      		badMatrix.times(baseMatrix);
+	  	} 
+		catch (IllegalArgumentException e) {
+	      		// Should be thrown!
+	      		result1 = true;
+	  	} 
+		catch (Exception e) {
+	      		// Uh oh. Something unexpectedly bad happened...
+	      		assertTrue(false);
+	  	}
+
+		assertTrue(result1);
+	}
+}
